0895519a356502e908a81a26863d9cfa28801b01
Mardjona <Mardjona26@gmail.com>
2024-08-17 17:13:46 +0300

    Initial commit

---
+++ MyKanban/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MyKanban.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ MyKanban/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyKanban.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
